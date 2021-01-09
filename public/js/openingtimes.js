812640a300482b8ca7857362e3dd717a8216cca7
Janne Cadamuro <janne.unger@gmail.com>
2021-01-09 21:53:30 +0100

    Fertig nach Modification von public-Dateien

--- public/js/openingtimes.js
+++ public/js/openingtimes.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var weekday = new Date().getDay();
   // Mo - Fr
-  if (weekday >= 1 && weekday <= 7) {
+  if (weekday >= 1 && weekday <= 5) {
     $('.opening-box').eq(weekday-1).addClass('today');
   }
 });
